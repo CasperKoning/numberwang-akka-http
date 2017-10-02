bfa44214eed0a98793f3b84a86f4fc369a0dd846
Casper Koning <casper.e.koning@gmail.com>
2017-10-02 22:44:19 +0200

    Implement lifecycle methods

--- src/main/scala/xyz/casperkoning/numberwang/Main.scala
+++ src/main/scala/xyz/casperkoning/numberwang/Main.scala
@@ -1,10 +1,27 @@
 package xyz.casperkoning
 package numberwang
 
+import akka.actor.ActorSystem
+import akka.stream.ActorMaterializer
+import com.typesafe.config.ConfigFactory
+
+import scala.concurrent._
+import scala.concurrent.duration._
+
 object Main extends App {
-  val service = new NumberwangService()
+  implicit val config = ConfigFactory.load()
+  implicit val system = ActorSystem("numberwang", config)
+  implicit val executionContext = system.dispatcher
+  implicit val materializer = ActorMaterializer()
+  val service = new NumberwangService(config)
+
   service.start()
+
   Runtime.getRuntime.addShutdownHook(new Thread(){
-    override def run(): Unit = service.stop()
+    override def run(): Unit = {
+      service.stop()
+      materializer.shutdown()
+      Await.result(system.terminate(), 10.seconds)
+    }
   })
 }
--- src/main/scala/xyz/casperkoning/numberwang/NumberwangService.scala
+++ src/main/scala/xyz/casperkoning/numberwang/NumberwangService.scala
@@ -2,26 +2,29 @@ package xyz.casperkoning
 package numberwang
 
 import akka.actor._
+import akka.http.scaladsl.Http.ServerBinding
 import akka.stream._
 import akka.http.scaladsl._
 import com.typesafe.config._
 
-class NumberwangService(val config: Config = ConfigFactory.load())
+import scala.concurrent._
+import scala.concurrent.duration._
+
+class NumberwangService(val config: Config)(implicit actorSystem: ActorSystem, executionContext: ExecutionContext, materializer: Materializer)
   extends ApiRoutes {
+  private var serverBinding: Option[ServerBinding] = None
 
   def start() = {
-    implicit val system = ActorSystem("numberwang", config)
-    implicit val materializer = ActorMaterializer()
-
     Http().bindAndHandle(
       handler = routes,
       interface = config.getString("http.interface"),
       port = config.getInt("http.port")
-    )
+    ) onSuccess {
+      case binding => serverBinding = Some(binding)
+    }
   }
 
   def stop() = {
-    // Unbind
-    // Terminate ActorSystem
+    serverBinding.foreach {b => Await.result(b.unbind(), 10.seconds) }
   }
 }
