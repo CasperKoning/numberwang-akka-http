2705e75e9a2c6e33835989678ba26341e630d044
Casper Koning <casper.e.koning@gmail.com>
2017-12-13 13:46:30 +0100

    Implement some more routes

--- src/main/scala/xyz/casperkoning/numberwang/ApiRoutes.scala
+++ src/main/scala/xyz/casperkoning/numberwang/ApiRoutes.scala
@@ -1,12 +1,37 @@
 package xyz.casperkoning
 package numberwang
 
+import scala.concurrent._
+import scala.util._
+
 import akka.http.scaladsl.server.Directives._
 
 trait ApiRoutes {
-  val routes = get {
-    complete {
-      "That is Numberwang!"
+  def routes()(implicit ec: ExecutionContext) = 
+    path("get") {
+     get {
+      complete {
+        "That was a GET"
+      }
+     }
+    } ~
+    path("post_sync") {
+      post {
+        complete {
+          "That was a synchronous POST"
+        }
+      }
+    } ~ 
+    path("post_async") {
+      post {
+        onComplete(doAsynchronousWork()) {
+          case Success(_) ⇒ complete("That was an asynchronous POST")
+          case Failure(ex) ⇒ complete("We failed to process your asychronous POST")
+        }
+      }
     }
+
+  private def doAsynchronousWork()(implicit ec: ExecutionContext): Future[Unit] = Future {
+    (): Unit
   }
 }
